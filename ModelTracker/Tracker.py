edc54fb18fb12784faa7e5d7beff48d1af2e71f5
Mohamed El-Kalioby <mkalioby@mkalioby.com>
2016-06-13 14:56:53 +0200

    Supporting Classes as values

--- ModelTracker/Tracker.py
+++ ModelTracker/Tracker.py
@@ -7,6 +7,7 @@ class ModelTracker(models.Model):
         self.old_state = self.__dict__.copy()
 
     def save(self, username, force_insert=False, force_update=False, using=None, update_fields=None):
+        types=[type("a"),type(1),type({}),type([]),type(("1",2)),type(True),type(1L),type(u"a"),type(1.1),type(None)]
         history = History()
         history.table = self._meta.db_table
         history.done_on = timezone.now()
@@ -19,9 +20,33 @@ class ModelTracker(models.Model):
             history.old_state = {}
         else:
             history.old_state=self.old_state
+        keys2del=[]
+        for key in history.old_state:
+            if type(history.old_state[key]) not in types:
+                if hasattr(history.old_state[key],"toJSON"):
+                    history.old_state[key]=history.old_state[key].toJSON()
+                elif hasattr(history.old_state[key],"pk"):
+                    history.old_state[key]= history.old_state[key].pk
+                else:
+                    keys2del.append(key)
+        for key in keys2del:
+            del history.old_state[key]
+        keys2del=[]
+        for key in history.new_state:
+            if type(history.new_state[key]) not in types:
+                if hasattr(history.new_state[key], "toJSON"):
+                    history.new_state[key] = history.new_state[key].toJSON()
+                elif hasattr(history.new_state[key], "pk"):
+                    history.new_state[key] = history.new_state[key].pk
+                else:
+                    keys2del.append(key)
+        for key in keys2del:
+            del history.new_state[key]
 
         models.Model.save(self,force_insert=force_insert,force_update=force_update,using=using,update_fields=update_fields)
         history.primary_key=self.pk
         history.new_state.pop("_state","")
         history.old_state.pop("_state","")
         history.save()
+    class Meta:
+        abstract =True
