892f91868cd9491b1a8d05122b963c0d5c496718
Mohamed El-Kalioby <mkalioby@mkalioby.com>
2016-06-16 18:05:52 +0200

    Moved to egg files

--- setup.py
+++ setup.py
@@ -1,13 +1,34 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+#from distutils.core import setup
 #from setuptools import find_packages
 
-setup(name='DjangoModelTracker',
+"""setup(name='DjangoModelTracker',
       version='0.2',
       description='Track Django Model Objects over time',
       author='Mohamed El-Kalioby',
       author_email='mkalioby@mkalioby.com',
       url='https://github.com/mkalioby/ModelTracker/',
       packages=('ModelTracker','ModelTracker.migrations'),
-   )
+
+   )"""
+
+from setuptools import find_packages, setup
+
+setup(
+    name='DjangoModelTracker',
+    version='0.3',
+    description='Track Django Model Objects over time',
+    author='Mohamed El-Kalioby',
+    author_email = 'mkalioby@mkalioby.com',
+    url = 'https://github.com/mkalioby/ModelTracker/',
+
+    download_url='https://github.com/mkalioby/ModelTracker/',
+    license='MIT',
+    packages=find_packages(exclude=('TestApp.*', 'TestApp', 'example')),
+    install_requires=[
+        'Django>=1.7',
+      ],
+    include_package_data=True,
+      zip_safe=False, # because we're including static files
+)
