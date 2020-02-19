a88193020cfd7cbeb0c24e676d54e9bc258f2c44
Mohamed ElKalioby <melkalioby@kfshrc.edu.sa>
2020-02-19 13:54:20 +0300

    Working with Django 2.2

--- ModelTracker/middleware.py
+++ ModelTracker/middleware.py
@@ -1,5 +1,19 @@
 from ModelTracker.Tracker import ModelTracker
-class ModelTrackerMiddleware(object):
+try:
+    from django.utils.deprecation import MiddlewareMixin
+except ImportError:  # Django < 1.10
+    # Works perfectly for everyone using MIDDLEWARE_CLASSES
+    MiddlewareMixin = object
+
+class ModelTrackerMiddleware(MiddlewareMixin):
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        ModelTracker.thread.request = request
+        response = self.get_response(request)
+        return response
+
     def process_request(self, request):
         ModelTracker.thread.request = request
 
