8283f5af5bfff19b1a681b85e34db3902961a23c
Mohamed ElKalioby <mkalioby@mkalioby.com>
2022-12-19 14:12:09 +0300

    Final Fixes to Django 4.0+

--- ModelTracker/urls.py
+++ ModelTracker/urls.py
@@ -1,9 +1,12 @@
-from django.conf.urls import url,re_path
+try:
+    from django.urls import re_path
+except ModuleNotFoundError:
+    from django.conf.urls import re_path
 from . import views
 
 urlpatterns = [
-    url('^$',views.main,name="main"),
-    url('show/',views.showChanges,name="showModelChanges"),
+    re_path('^$',views.main,name="main"),
+    re_path('show/',views.showChanges,name="showModelChanges"),
     re_path(r'revert/(\d+)',views.revert,name="revert"),
-    url('get/',views.getChanges,name="getModelChanges"),
-                       ]
+    re_path('get/',views.getChanges,name="getModelChanges"),
+   ]
--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='django-model-tracker',
-    version='2.0',
+    version='2.0.2',
     description='Track Django Model Objects over time',
     author='Mohamed El-Kalioby',
     author_email = 'mkalioby@mkalioby.com',
