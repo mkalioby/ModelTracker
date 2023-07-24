958e66c0b6e039a6efeec74bee42d9477d945340
Mohamed ElKalioby <mkalioby@mkalioby.com>
2023-07-24 17:52:36 +0300

    Fixed Middleware

--- ModelTracker/middleware.py
+++ ModelTracker/middleware.py
@@ -1,7 +1,12 @@
 from ModelTracker.Tracker import ModelTracker
+
+
 class ModelTrackerMiddleware(object):
-    def process_request(self, request):
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
         ModelTracker.thread.request = request
+        response = self.get_response(request)
 
-
-
+        return response
